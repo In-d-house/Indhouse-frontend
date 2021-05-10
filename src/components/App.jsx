9ac4ee0feef9ee97c905b6e42f791192f5f71166
yunsu <than4502@gmail.com>
2021-05-10 17:11:57 +0900

    feat: logout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Switch, Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 import Signup from "./Signup";
 import Login from "./Login";
+import Logout from "./Logout";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -11,6 +13,7 @@ const App = () => {
     <div>
       <Login />
       <Signup />
+      <Logout />
     </div>
   );
 };
